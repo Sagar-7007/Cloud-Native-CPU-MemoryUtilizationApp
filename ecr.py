c96eb8be9657a292d8b47545c706370b81f519c8
Sagar Kumar <82771516+Sagar-7007@users.noreply.github.com>
2024-04-13 17:35:05 +0200

    Add files via upload

---
+++ ecr.py
@@ -0,0 +1,11 @@
+##programmer Sagar_Kumar
+
+import boto3
+
+ecr_client = boto3.client('ecr')
+
+repository_name = "my_monitoring_app_image"
+response = ecr_client.create_repository(repositoryName=repository_name)
+
+repository_uri = response ['repository']['repositoryUri']
+print(repository_uri)
